ecc83c17e21abe47d8158ae9f8ef7285f48ae987
Лиза <lizakrvtsk@gmail.com>
2023-10-11 01:14:00 +0300

    о

---
+++ Лабораторная №3/Лаб_3_задание_3.py
@@ -0,0 +1,20 @@
+import re
+
+with open('subj.txt', 'r', encoding='utf-8') as file:
+    lines = file.readlines()
+
+subj_dict = {}
+details = []
+
+for line in lines:
+    parts = lines.strip().split(':')
+    if len(parts) == 2:
+        subj = parts[0]
+        details = parts[1] #chasy
+
+    num = re.findall(r'\d+', details)
+
+    total = sum(map(int, num))
+    subj_dict[subj] = total
+
+print(subj_dict)
